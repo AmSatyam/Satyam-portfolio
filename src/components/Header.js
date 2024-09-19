c2db12e814c0a5951ca0a9c4bc05b4019b4d463c
Satyam <satyamsaurav2001@gmail.com>
2024-09-20 00:04:44 +0530

    Updated logo size

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from "../assets/logo.svg"
 
 const Header = () => {
-  return <header className=' py-8' >
+  return <header className='py-0' >
 <div className='container mx-auto' >
   <div className='flex mt-1 justify-between items-center'>
     {/* logo  */}
